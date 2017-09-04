7b5565814a39c4a6fdc4305c9ecc650423507c39
Mounika1494 <edulamounika08@gmail.com>
2017-09-03 18:57:43 -0600

    not working properly

--- HW1/circ_buffer.c
+++ HW1/circ_buffer.c
@@ -1,141 +1,122 @@
-#include<stdlib.h>
-#include<stdint.h>
-//uint8_t length=4;
-/**** adding the data to the circular buffer***/
-/*uint8_t add_data(structure *s,uint8_t Value,uint8_t length)
-{
-/*** check before writing the values whether the buffer is full****/
-//if(Buffer_Full(s,length))
-//{
-//printf("Buffer is full");
-//return -1;
-//}
-
-//*s->HEAD++= Value;/*** writing the value onto the HEAD**/
- //s->HEAD++;
-//return 0;
-//}*/
-/****Buffer full****/
-/*uint8_t Buffer_Full(structure *s,uint8_t length)
-{
-if(s->HEAD==(s->Buffer + (length-1)*(sizeof(uint8_t))))
-{
-s->HEAD=s->Buffer;
-	return 1;
-}
-else
-return 0;
-}*/
-/****Buffer Empty****/
-/*uint8_t Buffer_Empty(structure *s)
-{
-if((s->HEAD)==(s->TAIL))
-return 1;
-else
-return 0;
-}*/
-/**** Buffer Remove*****/
-/*uint8_t remove_data(structure *s)
-{
-if(Buffer_Empty(s))
-{
-//printf("Buffer is Empty");
-return -1;
-}
-else
-{
-s->TAIL++;
-return 0;
-}
-}
-uint8_t get_data(structure *s,uint8_t length)
-{
-/*** check before writing the values whether the buffer is full****/
-/*if(Buffer_Empty(s))
-{
-//printf("Buffer is Empty");
-return -1;
-}
-if(s->TAIL==(s->Buffer + (length-1)*(sizeof(uint8_t))))
-{
-	s->TAIL=s->Buffer;
-}
-//s->HEAD--;
-return(*s->TAIL++);/*** writing the value onto the HEAD**/
-//s->HEAD++;
-// return 0;
-//}
-typedef enum
-{
-	false = 0, 
-	true =1
-}bool;
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 typedef enum  
 {
 	Success =0,
 	Fail = 1,
 	error
-}Status;
-Status allocate(void ** d_pointer);
-Status destroy();
-char *p_pointer;
-void main()
-{	
-	//enum Status state;
-	allocate(&p_pointer);
-}
-Status allocate(void ** d_pointer)
+}status;
+typedef struct {
+	uint8_t* HEAD;
+	uint8_t* TAIL;
+	uint8_t* BASE;
+	uint8_t  SIZE;
+	uint16_t NO_OF_ITEMS;
+	      }Buffer;
+Buffer *ptr;
+Buffer mybuffer;
+Buffer *ptr = &mybuffer;
+status allocate(Buffer ** d_pointer);
+status destroy();
+bool Is_buffer_full();
+bool Is_buffer_empty();
+status add(uint8_t data);
+status remove_item();
+void dump();
+uint8_t size();
+
+status allocate(Buffer ** d_pointer)
 {
 	if(d_pointer == NULL)
 	{
-		return error;
+	printf("error");
+ 	return error;
 	}
-	typedef struct {
-			uint8_t* HEAD;
-			uint8_t* TAIL;
-			uint8_t* BASE;
-			uint8_t  SIZE;
-			uint16_t NO_OF_ITEMS;
-			}Buffer;
-	 Buffer ** mybuffer;
-	 mybuffer =(Buffer **) d_pointer;
-        (*mybuffer)->SIZE = 50;
+        (*d_pointer)->SIZE = 50;
 	 char *memory;
-	memory = (char*)malloc(sizeof(char)*((*mybuffer)->SIZE));
+	memory = (char*)malloc(sizeof(char)*((*d_pointer)->SIZE));
 	if(memory == NULL)
 	{
-		return Fail;
+	printf("fail");	
+        return Fail;
 	}
-	(*mybuffer)->BASE = memory;
-	(*mybuffer)->HEAD = memory;
-	(*mybuffer)->TAIL = memory;
-        printf("Circular Buffer Base is at %d",(*mybuffer)->BASE);
-        printf("Circular Budder Head is ar %d",(*mybuffer)->HEAD);
-        printf("Circular Buffer Tail is at %d",(*mybuffer)->TAIL);
+	(*d_pointer)->BASE = memory;
+	(*d_pointer)->HEAD = memory;
+	(*d_pointer)->TAIL = memory;
+        printf("success");
+        printf("Circular Buffer Base is at %d",(*d_pointer)->BASE);
+        printf("Circular Budder Head is ar %d",(*d_pointer)->HEAD);
+        printf("Circular Buffer Tail is at %d",(*d_pointer)->TAIL);
 	return Success;
 }
-Status destroy()
-{
-	free(&p_pointer);
-	return Success;
-}
-/*bool Is_buffer_full()
-{
-	if(s->HEAD==(s->Buffer + (length-1)*(sizeof(uint8_t))))
+
+bool Is_buffer_full()
+{	
+     if(ptr->HEAD==(ptr->BASE + (ptr->SIZE)*(sizeof(uint8_t))))
 	{
-	s->HEAD=s->Buffer;
-		return true;
+        ptr->HEAD=ptr->BASE;
+	return true;
 	}
 	else
 	return false;	
 }
-bool Is_buffer_empty()
+/*bool Is_buffer_empty()
 {
-	if((s->HEAD)==(s->TAIL))
+	if((ptr->HEAD)==(ptr->TAIL))
 	return true;
 	else
 	return false;	
 }*/
+status add(uint8_t data)
+{
+printf("data is %d", data);
+if(Is_buffer_full())
+{
+return error;
+}
+*(ptr->HEAD++) = data;
+ ptr->NO_OF_ITEMS=ptr->NO_OF_ITEMS+1;
+ printf("no of items is %d",ptr->NO_OF_ITEMS);
+return Success;
+}
+status remove_item()
+{
+/*
+if(Is_buffer_empty())
+{
+return error;
+}*/
+ptr->TAIL++;
+(ptr->NO_OF_ITEMS)--;
+}
+uint8_t size()
+{
+return ptr->NO_OF_ITEMS;
+}
+
+status destroy()
+{
+        free(ptr);
+	return Success;
+}
 
 
+void main()
+{       printf("main");
+        
+	allocate(&ptr);
+        add(5);
+        uint8_t s = size();
+        printf("size is %d",s);
+        add(6);
+        uint8_t s1 = size();
+        printf("size is %d",s);
+        add(7);
+        uint8_t s2 = size();
+        printf("size is %d",s);
+        //destroy();
+        //printf("size after free the memory is %d",ptr->SIZE);
+}
 
