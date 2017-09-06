7b0d5cdb793ddf44fa9f91588a0fcaa84ebbc209
Mounika1494 <edulamounika08@gmail.com>
2017-09-05 18:16:32 -0600

    complete working add a node at any index

--- HW1/double_ll.c
+++ HW1/double_ll.c
@@ -29,7 +29,7 @@ printf("new node address is at %d", new_node);
 struct node *traverse = *head;
 new_node -> data = item;
 new_node -> next = NULL;
-if (index ==0)
+if (*head ==NULL)
 {
 printf("linked list is empty\n");
 *head = new_node;
@@ -38,11 +38,19 @@ printf("data %d is added at index %d whose next is %d and previous is at %d\n",i
 }
 else
 {
-while(traverse->next !=NULL)
+for(int i=1;i<(index-1);i++)
+{
 traverse = traverse->next;
+}
+new_node->next = traverse->next;
 traverse->next = new_node;
 new_node->previous = traverse;
-printf("previous node is at %d and next is pointed to %d\n",traverse,traverse->next);
+if(new_node->next!=NULL)
+{
+new_node->next->previous=new_node;
+printf("next node is at address %d and next to %d and previous to %d\n",new_node->next,new_node->next->next,new_node->next->previous);
+}
+printf("previous node is at address %d and next to %d and previous to %d\n",traverse,traverse->next,traverse->previous);
 printf("data %d is added at index %d whose next is %d and previous is at %d\n",index,*(int*)(new_node->data),new_node->next,new_node->previous);
 }
 return Success;
@@ -51,14 +59,16 @@ return Success;
 void main()
 {
 uint32_t data = 20;
-add_node(&head,&data,0);
+add_node(&head,&data,1);
 data=30;
 printf("head is at address %d\n",head);
-add_node(&head,&data,1);
-printf("head is at address %d\n",head);
-data=40;
 add_node(&head,&data,2);
 printf("head is at address %d\n",head);
+data=40;
+add_node(&head,&data,3);
+printf("head is at address %d\n",head);
+data=50;
+add_node(&head,&data,1);
 }
 
 
