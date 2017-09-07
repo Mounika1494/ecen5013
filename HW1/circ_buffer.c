113f81c4323abec1e71b39790750325aef385956
Mounika1494 <edulamounika08@gmail.com>
2017-09-06 23:04:43 -0600

    added small changes

--- HW1/circ_buffer.c
+++ HW1/circ_buffer.c
@@ -2,35 +2,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+
 typedef enum  
 {
 	Success =0,
 	Fail = 1,
-	error
-}status;
-typedef struct {
+	Error
+}Status;
+
+typedef struct
+{
 	uint8_t* HEAD;
 	uint8_t* TAIL;
 	uint8_t* BASE;
 	uint8_t  SIZE;
-	uint16_t NO_OF_ITEMS;
-	      }Buffer;
+	uint16_t NO_OF_ITEMS
+}Buffer;
+
 Buffer* new_buffer = NULL;
 
-status allocate(Buffer ** d_pointer);
-status destroy();
+Status allocate(Buffer ** d_pointer);
+Status destroy();
 bool Is_buffer_full();
 bool Is_buffer_empty();
-status add(void *data);
-status remove_item();
+Status add(void *data);
+Status remove_item();
 void dump();
 uint8_t size();
 
-status allocate(Buffer ** d_pointer)
+Status allocate(Buffer ** d_pointer)
 {
+        char *memory;
         Buffer* mybuffer =  (Buffer*)malloc(sizeof(Buffer));
-        mybuffer->SIZE = 5;
-	 char *memory;
+        printf("Enter the size of circular buffer\n");
+        scanf("%d",&(mybuffer->SIZE));
+        printf("buffer size is %d\n",mybuffer->SIZE);
 	memory = (char*)malloc(sizeof(char)*(mybuffer->SIZE));
 	if(memory == NULL)
 	{
@@ -63,12 +69,11 @@ bool Is_buffer_empty()
 	else
 	return false;	
 }
-status add(void *data)
+Status add(void *data)
 {
         if(Is_buffer_full())
         {
         printf("Buffer is full looping back\n");
-        //return error;
         }
         printf("item added at address %d index  %d has %d\n",(new_buffer->HEAD),new_buffer->NO_OF_ITEMS,*(int*)data);
         *(new_buffer->HEAD++) = *(int*)data;
@@ -77,23 +82,24 @@ status add(void *data)
         new_buffer->NO_OF_ITEMS=new_buffer->SIZE;
         return Success;
 }
-status remove_item()
+Status remove_item()
 {
          if(Is_buffer_empty())
          {
          printf("Buffer Empty\n");
-         return error;
+         //return Error;
          }
          printf("item is removed at address %d\n",new_buffer->TAIL);
          new_buffer->TAIL++;
          new_buffer->NO_OF_ITEMS--;
+         return Success;
 }
 uint8_t size()
 {
          return new_buffer->NO_OF_ITEMS;
 }
 
-status destroy()
+Status destroy()
 {
          free(new_buffer->BASE);
          free(new_buffer);
@@ -103,12 +109,19 @@ status destroy()
 
 void dump()
 {
+         uint8_t *traverse;
+         traverse = malloc(sizeof(uint8_t));
+         traverse = new_buffer->TAIL;
+         if(traverse==NULL)
+         {
+         printf("Error in the address\n");
+         }
          for(int i=0;i<(new_buffer->NO_OF_ITEMS);i++)
          {
-         printf("buffer at address %d index  %d has %d\n",(new_buffer->TAIL),i,*(new_buffer->TAIL));
-         new_buffer->TAIL++;
-         if(new_buffer->TAIL == (new_buffer->BASE + (((new_buffer->SIZE))*(sizeof(uint8_t)))))
-         new_buffer->TAIL=new_buffer->BASE;
+         printf("buffer at address %d index  %d has %d\n",(traverse),i,*(traverse));
+         traverse++;
+         if(traverse == (new_buffer->BASE + (((new_buffer->SIZE))*(sizeof(uint8_t)))))
+         traverse=new_buffer->BASE;
          }
 }
 
