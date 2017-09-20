a73043d522f81a10d89618cbe0126f295d300409
Mounika1494 <edulamounika08@gmail.com>
2017-09-19 20:37:53 -0600

    added comments

--- linux-4.12.10/drivers/mytime/mytime.c
+++ linux-4.12.10/drivers/mytime/mytime.c
@@ -3,13 +3,28 @@
 #include <linux/init.h>
 #include <linux/timer.h>
 
+/**********************************************************************
+*@Filename:mytime.c
+*
+*@Description:Implemented a module which is triggered every 500ms
+*
+*@Author:Mounika Reddy Edula
+*@Date:09/19/2017
+*@compiler:GCC
+*@Debugger:GDB
+*@kernel:4.12.10
+**********************************************************************/
+//parameters for modinfo
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Mounika Reddy Edula");
 MODULE_DESCRIPTION("Trigger a timer every 500ms");
 MODULE_VERSION("0.1");
 
+//static as the count value should be stored
 static struct timer_list my_timer;
-static int count;
+static int32_t count;
+
+//callback when timer expires
 void my_timer_callback(unsigned long data)
 {
 count++;
@@ -18,6 +33,7 @@ my_timer.expires = jiffies+msecs_to_jiffies(500);
 add_timer(&my_timer);
 }
 
+//The module is started with init function
 static int __init timer_start(void)
 {
 printk(KERN_INFO "Loading timer module ....\n");
@@ -27,6 +43,7 @@ mod_timer(&my_timer, jiffies+msecs_to_jiffies(200));
 return 0;
 }
 
+//The module is exited with __exit function
 static void __exit timer_end(void)
 {
 del_timer(&my_timer);
